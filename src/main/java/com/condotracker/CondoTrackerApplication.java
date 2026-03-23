706fcc7eea36a206ecc1de5ba3cccd8c6170cde4
arshavin166 <lazardunjic13@gmail.com>
2026-03-23 19:12:57 +0100

    config

---
+++ src/main/java/com/condotracker/CondoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.condotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CondoTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CondoTrackerApplication.class, args);
+    }
+}
